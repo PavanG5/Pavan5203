c4d4cca09699a37c40413773eb95df2243767e2e
Pavan Krishna G <150824534+PavanG5@users.noreply.github.com>
2025-01-26 23:59:55 +0530

    Delete testline directory

--- testline/quiz_analysis_project/src/recommendations.py
+++
@@ -1,5 +0,0 @@
-def generate_recommendations(weak_areas):
-    recommendations = []
-    for topic in weak_areas.index:
-        recommendations.append(f"Revise {topic} to improve accuracy.")
-    return recommendations
